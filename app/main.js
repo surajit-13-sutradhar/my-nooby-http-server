3af35e69baefd4f08c832deb9ef25b7a60380830
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2025-04-17 23:31:19 +0530

    extracting url path

--- app/main.js
+++ app/main.js
@@ -6,7 +6,11 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", () => {
-        socket.write("HTTP/1.1 200 OK\r\n\r\n")
+        // socket.write("HTTP/1.1 200 OK\r\n\r\n")
+        console.log(data)
+        const path = data.toString().split(" ")[1]
+        const responseStatus = path === "/" ? "200 ok": "404 NOT found"
+        socket.write(`HTTP/1.1 ${responseStatus} \r\n\r\n`)
         socket.end() // close connection
     })
 
