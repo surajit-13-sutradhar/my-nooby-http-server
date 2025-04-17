00f481985f5a2823e2257fca58c9d5ae87963ca0
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2025-04-17 23:32:53 +0530

    first commit

--- app/main.js
+++ app/main.js
@@ -5,18 +5,20 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+    socket.on("close", () => {
+        socket.end()
+        server.close()
+    })
+
     socket.on("data", () => {
         // socket.write("HTTP/1.1 200 OK\r\n\r\n")
         console.log(data)
         const path = data.toString().split(" ")[1]
         const responseStatus = path === "/" ? "200 ok": "404 NOT found"
         socket.write(`HTTP/1.1 ${responseStatus} \r\n\r\n`)
-        socket.end() // close connection
     })
 
-    socket.on("close", () => {
-        socket.end()
-    })
+    
 
 })
 
