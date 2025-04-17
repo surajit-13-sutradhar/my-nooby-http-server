a268728e77e48642fb50c2e06e4e7dbd36a215db
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2025-04-17 23:47:16 +0530

    responding with body

--- app/main.js
+++ app/main.js
@@ -12,8 +12,20 @@ const server = net.createServer((socket) => {
         // socket.write("HTTP/1.1 200 OK\r\n\r\n")
         console.log(data)
         const path = data.toString().split(" ")[1]
-        const responseStatus = path === "/" ? "200 OK": "404 Not Found"
+        let responseStatus 
+        if(path = "/") responseStatus = "200 OK"
+        else if(path.includes("/echo/")) {
+            const content = path.split("/echo/")[1]
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
+        }
+        else responseStatus = "404 Not Found"
         socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`)
+    })
+
+    socket.on("error", (err) => {
+        console.error("Socket error:", err)
+        socket.end()
+        server.close()
     })
 })
 
